6e7147b2cb2db91b8fe85d06d6141e5ca3ff1639
Shyam Bihari Swami <sbswami18@gmail.com>
2019-12-29 14:43:22 +0530

    Connect History Api Fallback in Express

--- express.js
+++ express.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const portNumber = 80;
 const sourceDir = 'dist';
+const connectHistoryApiFallback = require('connect-history-api-fallback');
+
+app.use(connectHistoryApiFallback({
+  verbose: false,
+}));
 
 app.use(express.static(sourceDir));
+
+app.get(express.static(sourceDir));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, sourceDir));
+});
 
 app.listen(portNumber, () => {
   console.log(`Express web server started: http://0.0.0.0:${portNumber}`);
